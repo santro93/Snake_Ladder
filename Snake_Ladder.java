2bc9cba3b619be3729600b84008835a2f882fd04
santro93 <nsantosh435@gmail.com>
2022-03-14 21:14:34 +0530

    Welcome to Snake And Ladder program

---
+++ Snake_Ladder.java
@@ -0,0 +1,16 @@
+package snake_Ladder;
+
+
+
+public class Snake_Ladder {
+
+		public static void main(String[] args) {
+			System.out.println("welcome to Snake And ladder Program");	
+			
+		
+		
+		
+		
+		}
+
+}
