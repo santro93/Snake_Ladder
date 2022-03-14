1a93f4a9c7a58eb558d412f48198750d00337304
santro93 <nsantosh435@gmail.com>
2022-03-14 21:23:46 +0530

    Dice roll

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -7,7 +7,8 @@ public class Snake_Ladder {
 		public static void main(String[] args) {
 			System.out.println("welcome to Snake And ladder Program");	
 			int playerPosition = 0; //start position
-		
+			byte dice = (byte)(Math.random()*6 + 1 );
+			System.out.println("Dice Show: " + dice);
 		
 		
 		
