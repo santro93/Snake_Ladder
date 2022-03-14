6e872e4ee0ae531dca21790bac364936c7aee760
santro93 <nsantosh435@gmail.com>
2022-03-14 21:46:49 +0530

    Report of number of times dice roll to win & position after dice roll

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -7,9 +7,9 @@ public class Snake_Ladder {
 		public static void main(String[] args) {
 			System.out.println("welcome to Snake And ladder Program");	
 			int playerPosition = 0; //start position
+			int CountToWin = 0;
 			
-			
-			while (playerPosition < 100) {
+			while (playerPosition < 100 && playerPosition >=0) {
 				
 				byte dice = (byte)(Math.random()*6 + 1 );
 				System.out.println("Dice Show: " + dice);
@@ -24,6 +24,9 @@ public class Snake_Ladder {
 				case 1:
 					System.out.println("Player played & got a Ladder");
 					playerPosition = playerPosition + dice;
+					if (playerPosition > 100) {
+						playerPosition = playerPosition - dice;
+					}
 					break;
 					
 				default:
@@ -36,7 +39,11 @@ public class Snake_Ladder {
 			
 				}
 				System.out.println("Player is at position : " + playerPosition);
+				CountToWin++;	
 			}
+			
+			System.out.println("Number of times dice rolled to Win the Game: " + CountToWin);
+			
 		}
 
 }
