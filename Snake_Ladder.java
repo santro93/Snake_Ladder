2b831372e5443babdc2325f2683cdff2fd7c8f3d
santro93 <nsantosh435@gmail.com>
2022-03-14 21:29:31 +0530

    Players are No Play,Ladder or snake

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -10,7 +10,30 @@ public class Snake_Ladder {
 			byte dice = (byte)(Math.random()*6 + 1 );
 			System.out.println("Dice Show: " + dice);
 		
+			byte playerOptions = (byte)(Math.random()*3 );
+			
+			switch (playerOptions) {
+			
+			case 0:
+				System.out.println("Player choose to Not to Play");
+				playerPosition = playerPosition;
+				break;
+				
+			case 1:
+				System.out.println("Player played & got a Ladder");
+				playerPosition = playerPosition + dice;
+				break;
+				
+			default:
+				System.out.println("Player played & got a Snake");
+				playerPosition = playerPosition - dice;
+				if (playerPosition <= 0) {
+					playerPosition = 0;
+				}
+				break;	
 		
+			}
+			System.out.println("Player is at position : " + playerPosition);
 		
 		}
 
